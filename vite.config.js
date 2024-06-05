a43f60a59c267490dee6ff91c1566a06b9df4467
Dixa Sapariya <dixasapariya786@gmail.com>
2024-06-05 22:22:44 +0530

    set public path

--- vite.config.js
+++ vite.config.js
@@ -13,4 +13,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
+  publicPath: process.env.NODE_ENV === "production" ? "/weather-tracker/" : "/",
 })
